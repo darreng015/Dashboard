45a62c546a41d550c31815a4002f82acef6fd606
Darren Gomes <darreng18.me@gmail.com>
2023-05-11 15:03:23 +0400

    updated

--- test/second.test.js
+++ test/second.test.js
@@ -4,12 +4,36 @@ let expect = chai.expect;
 chai.use(chaiHttp);
 
 describe("Users Testing Api",() => {
-    it("Should return 200 for the users",(done) => {
+    it("Should return 200 for the user",(done) => {
         chai.request('http://localhost:1111')
         .get('/users')
         .then((res) => {
             expect(res).to.have.status(200)
-        }).then(done,done)
+            done()
+        })
+        .catch((err) =>{
+            throw err
+        })
+    })
+    it("Should return 404 for the users",(done) => {
+        chai.request('http://localhost:1111')
+        .get('/user')
+        .then((res) => {
+            expect(res).to.have.status(404)
+            done()
+        })
+        .catch((err) =>{
+            throw err
+        })
+    })
+    it("Should return 200 for the add user",(done) => {
+        chai.request('http://localhost:1111')
+        .post('/addUser')
+        .send({"name":"Test User", "City":"Test City", "isActive":false})
+        .then((res) => {
+            expect(res).to.have.status(200)
+            done()
+        })
         .catch((err) =>{
             throw err
         })
